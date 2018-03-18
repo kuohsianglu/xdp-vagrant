b7b0aa96450ef7e5e6af84c6ef63857fa6fb2299
tz70s <su3g4284zo6y7@gmail.com>
2018-03-18 19:11:44 +0800

    Fix up deprecated lxd installation via ppa.
    The PPA had been deprecated on lxd installation.
    Reference issue here: https://github.com/lxc/lxd/issues/4176

--- setup-apt.sh
+++ setup-apt.sh
@@ -13,8 +13,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D4284CDD
 echo "deb [trusted=yes] https://repo.iovisor.org/apt trusty kernel" | tee /etc/apt/sources.list.d/iovisor.list
 echo "deb [trusted=yes] https://repo.iovisor.org/apt/trusty trusty-nightly main" | tee -a /etc/apt/sources.list.d/iovisor.list
 
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
+apt-get install -y software-properties-common -t trusty-backports lxd lxd-client
 
 apt-get update
 apt-get upgrade -y
